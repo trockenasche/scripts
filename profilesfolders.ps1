0f757b5773781f4a1821562694da4a7cf4f59077
Murdoc Bates <trockenasche@gmail.com>
2014-05-15 14:10:44 +0200

    Create profilesfolders.ps1

---
+++ profilesfolders.ps1
@@ -0,0 +1,18 @@
+cd \\nas\Profiles\$env:username
+attrib +S .\Contacts
+attrib +S .\Videos
+attrib +S .\Searches
+attrib +S .\Music
+attrib +S '.\Saved Games'
+attrib +S .\Pictures
+attrib +S .\Favorites
+attrib +S .\Desktop
+attrib +S .\Downloads
+attrib +S .\Documents
+attrib +S .\Links
+attrib +S .\AppData
+cd .\AppData\Roaming\Microsoft\Windows
+attrib +S .\Libraries
+attrib +S .\SendTo
+attrib +S '.\Start Menu'
+attrib +S .\Recent
