7c1ef1db717ef16b9a9ee3758dd915c5569cb96f
Murdoc Bates <trockenasche@gmail.com>
2017-10-02 15:02:57 +0200

    some fine tune

--- changer.bat
+++ changer.bat
@@ -1,5 +1,5 @@
-REM Frontmotion Firefox changer Version 1.2.0 by trockenasche
-@echo off
+REM Frontmotion Firefox changer Version 1.2.1 by trockenasche
+@echo on
 
 REM check for all proprietary tools
 IF NOT EXIST MsiFiler.exe GOTO :NOMSIFILER
@@ -16,21 +16,27 @@ IF EXIST %version% GOTO :EXIST
 :START
 REM extracting the msi file to folder
 msiexec /qb /a %filename% TARGETDIR="%cd%\%version%\"
+
 REM copy all change to the extracted folder
 xcopy /E /Y "change\*.*" "%version%\"
+
 REM change the icon in the exe-resources
 xcopy /y "%version%\Program Files\Mozilla Firefox\firefox.exe" "Icons\"
 cd Icons\
 ResHacker.exe -script iconchange.res
 cd ..
 move /y "Icons\firefox.exe" "%version%\Program Files\Mozilla Firefox\"
+
 REM MsiFiler populates the file table with file versions and sizes based on the copied file from the "change" directory
+ECHO MsiFiler is working...
 START /B "" "MsiFiler.Exe" -d "%version%\%filename%"
+REM the loop is just for the progressbar
 :LOOP
 <nul set /p "=."
 tasklist /nh /fi "imagename eq MsiFiler.Exe" 2>&1|findstr /b /i /l /c:"MsiFiler.Exe">nul ||GOTO :CONTINUE
 ping -n 1 localhost>nul
 GOTO :LOOP
+
 :CONTINUE
 xcopy /y firefox.mst %version%\
 IF EXIST %fileold% del %fileold%
@@ -50,25 +56,26 @@ If "%INPUT%"=="N" GOTO :EXIT
 ECHO ############  ERROR  ##############
 ECHO There is no MSI file
 ECHO ############  ERROR  ##############
-pause
-EXIT
+GOTO :EXIT2
 
 :NOMSIFILER
 ECHO ############  ERROR  ##############
 ECHO There is no MsiFiler.exe, please download it before you continue,
 ECHO and RTFM ;)
 ECHO ############  ERROR  ##############
-pause
-EXIT
+GOTO :EXIT2
 
 :NORES
 ECHO ############  ERROR  ##############
 ECHO There is no ResHacker.exe, please download it before you continue,
 ECHO and RTFM ;)
 ECHO ############  ERROR  ##############
-pause
-EXIT
+GOTO :EXIT2
 
 :EXIT
 ECHO ----finished! Go ahead, deploy!----
+:EXIT2
 pause
+SET version=
+SET filename=
+SET fileold=
